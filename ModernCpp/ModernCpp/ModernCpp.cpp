307c607029db47f59c935edf125110d0762b4255
aguspoles <aguspoles@gmail.com>
2021-05-16 21:19:33 -0300

    boost library for debugging types

--- ModernCpp/ModernCpp/ModernCpp.cpp
+++ ModernCpp/ModernCpp/ModernCpp.cpp
@@ -1,6 +1,29 @@
 #include "TemplateTypeDeduction.h"
+#include <vector>
+
+template<typename Container, typename Index>
+decltype(auto) accessElement(Container&& c, Index i)
+{
+    return std::forward<Container>(c)[i];
+}
 
 int main()
 {
     TemplateTypeDeduction::Example();
+
+    //auto type deduction is esentially the same as template type deduction except for the cases of initializer lists
+    auto c{ 1 }; //c = std::initializer_lis<int>
+    //TemplateTypeDeduction::ByValueFunction({ 1 }); //Won't compile can't deduce type for T
+    //auto f = []() { return { 3 }; } //Won't compile can't deduce type for T
+
+    //During template type deduction the referenc-ness of an initializing expresion is ignore. Wich is what auto do.
+    //decltype gives you the type you would expect
+    const int& number = 10;
+    auto number1 = number; // number1 = int
+    decltype(auto) number2 = number; // number2 = const int&
+
+    //operator [] of a container tipically returns a T&
+    std::vector<int> v{1,2,3};
+    accessElement(v, 0) = 8; //decltype saves this, otherwise we would be attempting to assign a value to an rvalue
+    auto value = accessElement(std::vector<int>{4}, 0); //The universal ref allow us to pass an rvalue to the function
 }
--- ModernCpp/ModernCpp/TemplateTypeDeduction.h
+++ ModernCpp/ModernCpp/TemplateTypeDeduction.h
@@ -1,19 +1,42 @@
 #pragma once
 #include <iostream>
+#include <boost/type_index.hpp>
+
+#define LOG_TYPE(T, name) std::cout << name << " = " << boost::typeindex::type_id_with_cvr<T>().pretty_name() << '\n';
 
 namespace TemplateTypeDeduction
 {
     template<typename T>
-    void ByValueFunction(T param) {}
+    void ByValueFunction(T param) 
+    {
+        std::cout << "ByValueFunction" << std::endl;
+        LOG_TYPE(T, "T");
+        LOG_TYPE(decltype(param), "ParamType");
+    }
 
     template<typename T>
-    void ByRefOrPointerFunction(T& param) {}
+    void ByRefOrPointerFunction(T& param) 
+    { 
+        std::cout << "ByRefOrPointerFunction" << std::endl;
+        LOG_TYPE(T, "T"); 
+        LOG_TYPE(decltype(param), "ParamType");
+    }
 
     template<typename T>
-    void ByConstRefOrPointerFunction(const T& param) {}
+    void ByConstRefOrPointerFunction(const T& param) 
+    {
+        std::cout << "ByConstRefOrPointerFunction" << std::endl;
+        LOG_TYPE(T, "T");
+        LOG_TYPE(decltype(param), "ParamType");
+    }
 
     template<typename T>
-    void ByUniversalRefFunction(T&& param) {}
+    void ByUniversalRefFunction(T&& param) 
+    {
+        std::cout << "ByUniversalRefFunction" << std::endl;
+        LOG_TYPE(T, "T");
+        LOG_TYPE(decltype(param), "ParamType");
+    }
 
     //Return the size of an array as a comile time constant
     template<typename T, std::size_t N>
